dc2b28069123551f00d8e1ca54a20abf2751685d
DotMatrixBone <ngnhatduc.troy10ahust@gmail.com>
2021-05-23 23:45:13 +0700

    Inital commit. ++Added solution for jump game (https://leetcode.com/problems/jump-game/)

---
+++ jump_game.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool canJump(vector<int>& nums)
+    {
+        int N = nums.size();
+        int maxIdx = 0;
+        for(int i = 0; i < N; i++)
+        {
+            if(maxIdx >= N - 1)
+                return true;
+            if(i > maxIdx)
+                return false;
+            maxIdx = (i + nums[i]) > maxIdx ? i + nums[i] : maxIdx;
+        }
+        return false;
+    }
+};
